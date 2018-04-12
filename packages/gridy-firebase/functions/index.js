8605c212de6a4ca7d9fe05999f16b227ad2483ec
darosh <ensonador@gmail.com>
2018-04-12 20:31:08 +0200

    onlineUsers fnc

--- packages/gridy-firebase/functions/index.js
+++ packages/gridy-firebase/functions/index.js
@@ -1,5 +1,44 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 const secureCompare = require('secure-compare')
+const cors = require('cors')
+
+
+const USER_LIMIT = 100
+
+admin.initializeApp()
+
+const corsHandler = cors({ origin: true });
+const db = admin.database()
+const usersRef = db.ref('users').orderByChild('online').equalTo(true)
+
+let onlineUsers = []
+let onlineCount = 0
+let onlineUsersRequestCount = 0
+let connectionReuseCount = 0
+let resolveUsers
+
+const usersPromise = new Promise(resolve => {
+  resolveUsers = resolve
+})
+
+usersRef.on('value', (snap) => {
+  const val = snap.val() || {}
+  const keys = Object.keys(val)
+  onlineCount = keys.length
+  connectionReuseCount++
+  onlineUsers = keys.filter((k, i) => i < USER_LIMIT).map(k => {
+    const i = val[k]
+    return {
+      name: i.name,
+      avatar: i.avatar
+    }
+  })
+
+  if (connectionReuseCount === 1) {
+    resolveUsers()
+  }
+})
 
 exports.hello = functions.https.onRequest((req, res) => {
   res.send('Hello from Firebase!')
@@ -15,4 +54,13 @@ exports.secretHello = functions.https.onRequest((req, res) => {
   }
 
   res.send('Secret hello from Firebase!')
+})
+
+exports.onlineUsers = functions.https.onRequest((req, res) => {
+  corsHandler(req, res, () => {
+    usersPromise.then(() => {
+      onlineUsersRequestCount++
+      res.json({ data: { onlineCount, connectionReuseCount, onlineUsersRequestCount, onlineUsers } })
+    })
+  })
 })
