b0c57115485fd58d22b8176f0a601d21d5aa5040
Jan Forst <ensonador@gmail.com>
2019-05-29 14:25:11 +0200

    [vuetify-svg-icon]

---
+++ packages/gridy-vuetify-svg-icon/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-icon v-text="'$vuetify.icons.twitter'"/>
+      <v-icon v-text="'$vuetify.icons.twitter'" color="blue" />
+      <v-icon v-text="'$vuetify.icons.google'"/>
+      <v-icon v-text="'$vuetify.icons.google'" :size="48" />
+    </v-content>
+  </v-app>
+</template>
---
+++ packages/gridy-vuetify-svg-icon/src/components/Icon.vue
@@ -0,0 +1,43 @@
+<!-- eslint-disable vue/no-v-html -->
+
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    @click="$emit('click', $event)">
+    <g
+      v-if="path[0] === '<'"
+      v-html="path" />
+    <path
+      v-else
+      :d="path" />
+  </svg>
+</template>
+<script>
+const DEFAULT_SIZE = 24
+
+export default {
+  props: {
+    path: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      size: this.$parent.size || DEFAULT_SIZE
+    }
+  },
+  watch: {
+    '$parent.size' (size) {
+      this.size = (size != null) ? size : DEFAULT_SIZE
+    }
+  }
+}
+</script>
+<style scoped>
+svg {
+  fill: currentColor;
+}
+</style>
